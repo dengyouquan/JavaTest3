04864b8095c2d26f5da7ddd049521298bf280182
dengyouquan <youquan.deng@hand-china.com>
2018-07-24 13:00:13 +0800

    [IMP] 修正docker-compose的停止方式

--- clear.sh
+++ clear.sh
@@ -1,7 +1,8 @@
-docker stop javatest3_mysql_1
-docker stop javatest3_jdk_1
-docker rm javatest3_mysql_1
-docker rm javatest3_jdk_1
+docker-compose down
+#docker stop javatest3_mysql_1
+#docker stop javatest3_jdk_1
+#docker rm javatest3_mysql_1
+#docker rm javatest3_jdk_1
 docker rmi mysql
 docker rmi java
 docker rmi $(docker images -q -f dangling=true)
